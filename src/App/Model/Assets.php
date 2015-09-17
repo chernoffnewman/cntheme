de67364d1d9100b90c7b92a7ba90d3c60a3b0b93
Jonathan Mayhak <jonathan.mayhak@chernoffnewman.com>
2015-09-17 14:07:01 -0400

    pull modernizer through bower, update twitter api sdk

--- src/App/Model/Assets.php
+++ src/App/Model/Assets.php
@@ -15,9 +15,9 @@ class Assets
 
     public static function enqueue_scripts()
     {
-        wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/bower_components/bootstrap/dist/js/bootstrap.min.js', array('jquery'));
-        wp_enqueue_script('respond-js', get_template_directory_uri() . '/bower_components/respond-minmax/dest/respond.min.js');
-        wp_enqueue_script('modernizr-js', get_template_directory_uri() . '/bower_components/modernizr/modernizr.js');
+        wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/bower_components/bootstrap/dist/js/bootstrap.min.js', array('jquery'), false, true);
+        wp_enqueue_script('respond-js', get_template_directory_uri() . '/bower_components/respond-minmax/dest/respond.min.js', array(), false, true);
+        wp_enqueue_script('modernizr-js', get_template_directory_uri() . '/bower_components/modernizr/modernizr.js', array(), false, true);
 
         wp_register_script('jquery-quicksearch-js', get_template_directory_uri() . '/js/lib/jquery.quicksearch.js', array('jquery'), Config::getAppVersion(), true);
     }

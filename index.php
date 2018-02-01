9bff94eb4354f3a73e485c958c47440fd08aee3c
Jay Barry <jay.barry@chernoffnewman.com>
2018-02-01 10:49:11 -0500

    Move gitignore to root

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-$context = Timber::get_context();
-$context['post'] = new TimberPost();
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
 
-Timber::render('page-default.twig', $context);
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
